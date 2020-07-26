3142d890bcf4271eb06c3114aae8326e827f4c21
dayserivera <dayserivera@gmail.com>
2020-07-26 19:11:45 -0300

    first commit

---
+++ src/main/java/com/techfeense/virtualstoreorder/VirtualStoreOrderApplication.java
@@ -0,0 +1,13 @@
+package com.techfeense.virtualstoreorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStoreOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualStoreOrderApplication.class, args);
+	}
+
+}
