0983493399b39a377aadbc5f1cc8f1799024db98
dayserivera <dayserivera@gmail.com>
2020-07-27 23:00:06 -0300

    some refactoring

--- src/main/java/com/techfeense/virtualstoreorder/controllers/OrderController.java
+++ src/main/java/com/techfeense/virtualstoreorder/controllers/OrderController.java
@@ -10,6 +10,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -129,8 +130,8 @@ public class OrderController {
 		return new ResponseEntity<>(returnValue, HttpStatus.CREATED);
 	}
 	
-	@RequestMapping("/items")
-	@PutMapping
-	
-	
+	/*@RequestMapping("/items")
+	@DeleteMapping
+	public ResponseEntity<> removeItemFromOrder(@Valid)
+	*/
 }
